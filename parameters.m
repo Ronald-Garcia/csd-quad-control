acff93cbc86e1f7fc6d4462dd571ec8718c48473
Ronald-Garcia <rgarcia0303757@gmail.com>
2025-11-19 11:47:55 -0500

    Added UAV, waypoints

--- parameters.m
+++ parameters.m
@@ -15,6 +15,9 @@ p.Iyy       = p.Ixx;    % kg-m^2
 p.Izz       = 0.0282;   % kg-m^2 https://arxiv.org/pdf/2202.07021
 p.kD        = 0.25;     % kg/s
 
+p.dThres    = 0.01;     % m 
+p.omegaMax = 500^2;     % rad/s
+
 A = [0,0,0,1,0,0,0,0,0,0,0,0;...
      0,0,0,0,1,0,0,0,0,0,0,0;...
      0,0,0,0,0,1,0,0,0,0,0,0;...
@@ -36,25 +39,27 @@ C(1:3, 1:3) = eye(3);
 C(4:6, 7:9) = eye(3);
 D = zeros(size(C,1),4);
 
-x_K = -3;
-y_K = -4;
-z_K = -0.4;
-dotx_K = -1.4;
-doty_K =-1.5;
-dotz_K =-1.6;
-phi_K = -7;
-theta_K =-8;
-psi_K = -9;
-dotphi_K = -10;
-dottheta_K = -11;
-dotpsi_K = -12;
+x_K = -4.0;
+y_K = -1.8;
+z_K = -1.8;
+dotx_K = -4.02;
+doty_K =-3.03;
+dotz_K =-3.01;
+phi_K = -2.3;
+theta_K =-2.4;
+psi_K = -2.5;
+dotphi_K = -1.11;
+dottheta_K = -1.62;
+dotpsi_K = -1.63;
 
-K = place(A, B, [x_K,y_K,z_K,dotx_K,doty_K,dotz_K,phi_K,theta_K,psi_K, dotphi_K,dottheta_K,dotpsi_K]);
-L = place(A', C', [-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16])';
+eig_K_c = [x_K,y_K,z_K,dotx_K,doty_K,dotz_K,phi_K,theta_K,psi_K, dotphi_K,dottheta_K,dotpsi_K];
+eig_L_c = [-5,-3.1,-3.2,-5.1,-5.2,-5.3,-2.6,-2.5,-2.4,-2.3,-2.2,-2.1];
+K = place(A, B, eig_K_c);
+L = place(A', C', eig_L_c)';
 
 lin_sys = ss(A,B,C,D);
 
-dt = 0.1;
+dt = 0.01;
 
 dlin_sys = c2d(lin_sys, dt);
 
@@ -63,5 +68,14 @@ Bd = dlin_sys.B;
 Cd = dlin_sys.C;
 Dd = dlin_sys.D;
 
-Kd = place(Ad, Bd, [0.01,0.13,0.2,0,0.3,0,0.1,0,0.022,0.33,0.22,0.11]);
-Ld = place(Ad', Cd', [0.01,0.13,0.2,0,0.3,0,0.1,0,0.022,0.33,0.22,0.11])';
+Kd = place(Ad, Bd, exp(eig_K_c * dt));
+Ld = place(Ad', Cd', exp(eig_L_c * dt))';
+
+bL = p.b * p.L;
+Lk = p.L * p.k;
+b2 = 2*p.b;
+u2omega = 1/(4*p.b * p.k * p.L) * [bL, b2, 0, Lk; bL, 0, b2, -Lk; bL, -b2, 0, Lk; bL, 0, -b2, -Lk];
+omega2u = [ones(1,4) * p.k; Lk, 0, -Lk, 0; 0, Lk, 0, -Lk; p.b, -p.b, p.b, -p.b];
+
+
+p.wp = [[8,3,5]', [7,9,10]', [5,1,1]', [3,8,3]', [8,2,9]', [3;2;3], [6;5;4], [9;6;5]];
